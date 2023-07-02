58b2e3b352515b7da39263785b54ca740b5bbcb0
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-07-02 21:59:46 +0100

    Create AddBooks() Function
    Add multiple books specified as function arguments to the myLibrary
    array.

--- script.js
+++ script.js
@@ -24,7 +24,16 @@ function displayAllBooks() {
         console.log(book.info())
     })
 }
+
 const book1 = new Book('hobbit', 'jk tolkien', '420', 'no')
 const book2 = new Book('sound of waves', 'mishima i think', '420', 'no')
 const book3 = new Book('asya', 'ivan turganev', '420', 'no')
 const book4 = new Book('murder of roget ackroyd', 'agatha christie', '420', 'no')
+
+function addBooks(...books) {
+    for (book of books) {
+        console.log(book.info())
+    }
+}
+
+addBooks(book1, book2, book3, book4);
