e81c24f553a91ab95bb3bbd0ef3c9df71f8e2113
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-07-02 18:44:02 +0100

    Create Book Factory Function
    Book objects contain title, author, number of pages, and if read.

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('h');
+// module
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+    // constructor
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+Book.prototype.info = function() {
+    return (this.title)
+
+}
+
+
+// controller
+function addBookToLibrary() {
+
+}
